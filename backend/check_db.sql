ea36c244ce632c9e7772a9578706f180f465fb9b
yeshi-2001 <yeshikabandara2001@gmail.com>
2026-03-11 23:27:23 +0530

    Update requirements.txt and add database check script

---
+++ backend/check_db.sql
@@ -0,0 +1,17 @@
+-- List all databases
+\l
+
+-- Connect to skillmate_db
+\c skillmate_db
+
+-- List all tables
+\dt
+
+-- Show table structures
+\d users
+\d skills
+\d user_skills
+\d matches
+\d messages
+\d sessions
+\d ratings
